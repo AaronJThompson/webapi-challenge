d0e2928dc263c581513e0d20192a36ad43f71754
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-12 11:00:27 +0100

    Stretch: Add cors

--- api/server.js
+++ api/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const projectRoutes = require('./projects/routes');
 const actionRoutes = require('./actions/routes');
 const server = express();
-
+const cors = require('cors')
 server.use(express.json());
-
+server.use(cors());
 server.use('/api/projects', projectRoutes);
 
 server.use('/api/actions', actionRoutes);
