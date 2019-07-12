8e5311bef59797519336544b25c77aef0f5204d0
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-12 09:11:13 +0100

    Add get all projects endpoint

--- projects/routes.js
+++ projects/routes.js
@@ -4,5 +4,16 @@ const helmet = require('helmet');
 const projectsDB = require('../data/helpers/projectModel');
 
 router.use(helmet());
+function sendError (res, message = `Server couldn't complete request`, code = 500) {
+    res.status(code).json({ error: message });
+}
+router.get('/', async (req, res) => {
+    try {
+        const projs = await projectsDB.get();
+        res.status(200).json(projs);
+    } catch (error) {
+        sendError(res, "Error retrieving projects");
+    }
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
-
+const projectRoutes = require('./projects/routes');
+const actionRoutes = require('./actions/routes');
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/projects', projectRoutes);
+
+server.use('/api/actions', actionRoutes);
 
 module.exports = server;
