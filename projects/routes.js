ba081a530a8c7c4ef51d164ac546dd9047240a3f
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-12 09:17:02 +0100

    ValidateProjectID middleware added

--- projects/routes.js
+++ projects/routes.js
@@ -4,8 +4,28 @@ const helmet = require('helmet');
 const projectsDB = require('../data/helpers/projectModel');
 
 router.use(helmet());
+
 function sendError (res, message = `Server couldn't complete request`, code = 500) {
     res.status(code).json({ error: message });
+}
+
+async function validateProjectID(req, res, next) {
+    if (req.params.id) {
+        const id = new Number(req.params.id);
+        if(!isNaN(id)) {
+            try {
+                req.project = await projectsDB.get(id);
+                next();
+                return;
+            } catch (error) {
+                sendError(res, `Project id ${id} does not exist`, 404);
+                return;
+            }
+        }
+        sendError(res, `${req.params.id} is not a valid id`, 400);
+    } else {
+        next();
+    }
 }
 router.get('/', async (req, res) => {
     try {
@@ -15,5 +35,7 @@ router.get('/', async (req, res) => {
         sendError(res, "Error retrieving projects");
     }
 })
+
+router.get('/:id')
 
 module.exports = router;
