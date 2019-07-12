5da81870270ac0f7f8e0a4e2f542da11c415943e
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-12 10:01:33 +0100

    Action middleware added

--- actions/routes.js
+++ actions/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const helmet = require('helmet');
 
 const actionsDB = require('../data/helpers/actionModel');
+const helper = require('../data/helpers/requestHelpers');
 
 router.use(helmet());
 
--- data/helpers/requestHelpers.js
+++ data/helpers/requestHelpers.js
@@ -41,3 +41,38 @@ async function validateProjectID(req, res, next) {
         next();
     }
 }
+
+async function validateActionID(req, res, next) {
+    if (req.params.id) {
+        const id = parseInt(req.params.id);
+        if(!isNaN(id)) {
+            try {
+                req.action = await actionsDB.get(id);
+                if (req.action === null || Object.keys(req.action).length === 0) {
+                    throw new Error("Action was empty or doesn't exist");
+                }
+                next();
+                return;
+            } catch (error) {
+                sendError(res, `Action id ${id} does not exist`, 404);
+                return;
+            }
+        }
+        sendError(res, `${req.params.id} is not a valid id`, 400);
+    } else {
+        next();
+    }
+}
+
+function validateAction(req, res, next) {
+    const { project_id, description, notes } = req.body;
+    if (!project_id || !description || !notes) {
+        sendError(res, "project-id, description and notes field are required to create action", 400);
+        return;
+    }
+    if (description.length > 128) {
+        sendError(res, "description too long. 128 character limit", 400);
+        return;
+    }
+    next();
+}
